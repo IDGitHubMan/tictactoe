b153857c3eae802f2046fc2bad25d4a6ea8b5fce
Glitch (tictactoesyep) <none>
2021-02-11 23:30:39 +0000

    ⛈🐦 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
+// True if x, false if o
+var currentPlayer = 'x';
+var playerBool = true;
+var turns = 0;
+var gameOver = false;
 
+function performLogic(b,t){
+  $(b).hide();
+  $(t).text(currentPlayer);
+  switchPlayer();
+}
 
-
-
-
+function switchPlayer(){
+  if (playerBool){
+    currentPlayer = 'o'
+  }
+  else{
+    currentPlayer = 'x'
+  }
+  playerBool = !playerBool;
+}
 
 
 $("#button1").click(function() {
